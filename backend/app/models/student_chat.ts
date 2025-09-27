40a8bfe7f41d1244ddadecd4150fe6a9aa043f85
aswinganga-greeninkpro <aswinganga.greeninkpro@gmail.com>
2025-09-27 22:29:54 +0530

    no backend and frontend

--- backend/app/models/student_chat.ts
+++
@@ -1,8 +0,0 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
-
-export default class StudentChat extends BaseModel {
-  @column({ isPrimary: true }) declare id: number
-  @column() declare userId: number
-  @column() declare question: string
-  @column() declare answer: string
-}
